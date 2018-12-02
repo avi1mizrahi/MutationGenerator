95b4def27b2a8c950ba0045860575173d97357db
Avi Mizrahi <avi1mizrahi@gmail.com>
2018-12-02 15:22:52 +0200

    BinaryExprMutatorTest

--- src/test/java/BinaryExprMutatorTest.java
+++ src/test/java/BinaryExprMutatorTest.java
@@ -1,39 +1,110 @@
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Modifier;
-import com.github.javaparser.ast.expr.*;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryExprMutatorTest {
 
-    @Test
-    void process() {
-        var cu = new CompilationUnit();
-        var type1 = cu.addClass("C1");
-        var type2 = cu.addClass("C2");
-        var foo = type1.addMethod("foo", Modifier.PUBLIC);
-        var foo2 = type2.addMethod("foo", Modifier.PUBLIC);
+    static private class MutationsCase {
+        private final MethodDeclaration orig;
+        private final Set<MethodDeclaration> expected;
 
-        foo.setBody(new BlockStmt());
-        foo2.setBody(new BlockStmt());
+        MutationsCase(String from, String... to) {
+            var cu1 = JavaParser.parse(from);
+            var cus = List.of(to).stream().map(JavaParser::parse).collect(Collectors.toSet());
 
-        var binaryExp = new BinaryExpr(new IntegerLiteralExpr(34), new IntegerLiteralExpr(88), BinaryExpr.Operator.MULTIPLY);
-        foo.getBody().get().addStatement(new ReturnStmt(binaryExp));
+            orig = cu1.findFirst(MethodDeclaration.class).get();
+            expected = cus.stream().map(cu -> cu.findFirst(MethodDeclaration.class).get() ).collect(Collectors.toSet());
+        }
 
-        var mutatedBinaryExp = new BinaryExpr(new IntegerLiteralExpr(88), new IntegerLiteralExpr(34), BinaryExpr.Operator.MULTIPLY);
-        foo2.getBody().get().addStatement(new ReturnStmt(mutatedBinaryExp));
+        public Set<String> expectedAsStrings() {
+            return expected.stream().map(Node::toString).collect(Collectors.toSet());
+        }
+    }
 
+    private static void checkExpectation(MutationsCase mutationsCase) {
         var binaryExprMutator = new BinaryExprMutator();
-        List<String> m = binaryExprMutator.process(foo);
+        var generatedMutations = Set.copyOf(binaryExprMutator.process(mutationsCase.orig));
 
-        assertEquals(1, m.size());
+        assertEquals(mutationsCase.expected.size(), generatedMutations.size());
 
-        assertNotEquals(foo.toString(), m.get(0));
-        assertEquals(foo2.toString(), m.get(0));
+        assertFalse(generatedMutations.contains(mutationsCase.orig.toString()));
+        assertEquals(mutationsCase.expectedAsStrings(), generatedMutations);
+    }
+
+    @Test
+    void flipIntegers() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { return 355 + 123; } }",
+                "class C { int foo() { return 123 + 355; } }"));
+    }
+
+
+    @Test
+    void notCommutative() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { return 3 - 11 * 4 / 5; } }",
+                "class C { int foo() { return 3 - 4 * 11 / 5; } }"));
+    }
+
+    @Test
+    void manyStatements() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { if (a > 5) return a ^ 4;" +
+                                      "if (b >= 6 || c < 4) return b - a; } }",
+                "class C { int foo() { if (5 < a) return a ^ 4;" +
+                                      "if (b >= 6 || c < 4) return b - a; } }",
+                "class C { int foo() { if (a > 5) return 4 ^ a;" +
+                                      "if (b >= 6 || c < 4) return b - a; } }",
+                "class C { int foo() { if (a > 5) return a ^ 4;" +
+                                      "if (6 <= b || c < 4) return b - a; } }",
+                "class C { int foo() { if (a > 5) return a ^ 4;" +
+                                      "if (b >= 6 || 4 > c) return b - a; } }",
+                "class C { int foo() { if (a > 5) return a ^ 4;" +
+                                      "if (c < 4 || b >= 6) return b - a; } }"));
+    }
+
+    @Test
+    void dontFlipStringLiterals() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { return \"355\" + (5 + 6) + \"123\"; } }",
+                "class C { int foo() { return \"355\" + (6 + 5) + \"123\"; } }"));
+    }
+
+    @Test
+    void dontFlipReturnedStrings() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { return simpleGetNameNow() + (6 + 5); } }",
+                "class C { int foo() { return simpleGetNameNow() + (5 + 6); } }"));
+    }
+
+    @Test
+    void flipReturnedNotStrings() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { return 5 + simpleGetIntNow(); } }",
+                "class C { int foo() { return simpleGetIntNow() + 5; } }"));
+    }
+
+    @Test
+    void comparingStrings() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { return thisIsString() != null && \"this too\".isEmpty(); } }",
+                "class C { int foo() { return null != thisIsString() && \"this too\".isEmpty(); } }",
+                "class C { int foo() { return \"this too\".isEmpty() && thisIsString() != null; } }"));
+    }
+
+    @Test
+    void twoLevel() {
+        checkExpectation(new MutationsCase(
+                "class C { int foo() { return 5 + 6 * 30 + 14; } }",
+                "class C { int foo() { return 6 * 30 + 5 + 14; } }",
+                "class C { int foo() { return 5 + 30 * 6 + 14; } }",
+                "class C { int foo() { return 14 + 5 + 6 * 30; } }"));
     }
 }
